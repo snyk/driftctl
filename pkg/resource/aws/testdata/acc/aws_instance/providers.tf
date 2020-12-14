f4714af0cc44ae51b4f52107714ee602ba4532d5
Elie <elie.charra@cloudskiff.com>
2020-12-14 10:49:12 +0100

    Fix acceptance tests
    - Better terraform error output
    - Remove terraform version constraint in test files

--- pkg/resource/aws/testdata/acc/aws_instance/providers.tf
+++ pkg/resource/aws/testdata/acc/aws_instance/providers.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_version = "= 0.12.29"
-  required_providers {
-    aws    = "= 3.5.0"
-    random = "= 3.0.0"
-  }
-}
-
 provider "aws" {
-  version = "3.5.0"
+  version = "3.19.0"
   region  = "eu-west-3"
 }
--- test/acceptance/testing.go
+++ test/acceptance/testing.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/cloudskiff/driftctl/pkg/analyser"
+	"github.com/pkg/errors"
 
 	"github.com/sirupsen/logrus"
 
@@ -93,17 +94,17 @@ func terraformApply() error {
 
 	logrus.Debug("Running terraform init ...")
 	cmd := exec.Command("terraform", "init")
-	err := cmd.Run()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return err
+		return errors.Wrap(err, string(out))
 	}
 	logrus.Debug("Terraform init done")
 
 	logrus.Debug("Running terraform apply ...")
 	cmd = exec.Command("terraform", "apply", "-auto-approve")
-	err = cmd.Run()
+	out, err = cmd.CombinedOutput()
 	if err != nil {
-		return err
+		return errors.Wrap(err, string(out))
 	}
 	logrus.Debug("Terraform apply done")
 
