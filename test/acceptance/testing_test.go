dcee6fd6b341063ad3d025bfa7ec7efc14886ffa
William Beuil <william.beuil@gmail.com>
2021-02-04 22:06:09 +0100

    Add retry mechanism for polling, use it to poll sqs queues during acc tests

--- test/acceptance/testing_test.go
+++ test/acceptance/testing_test.go
@@ -1,10 +1,15 @@
 package acceptance
 
 import (
+	"context"
+	"errors"
 	"os"
 	"reflect"
 	"sort"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAccTestCase_resolveTerraformEnv(t *testing.T) {
@@ -28,4 +33,47 @@ func TestAccTestCase_resolveTerraformEnv(t *testing.T) {
 		t.Fatalf("Variable env override not working, got: %+v, expected %+v", env, expected)
 	}
 
+}
+
+func TestRetryFor(t *testing.T) {
+	randomError := errors.New("random error")
+
+	cases := []struct {
+		name    string
+		timeout time.Duration
+		f       func(c chan struct{}) error
+		err     error
+	}{
+		{
+			name:    "success on first try",
+			timeout: 1 * time.Millisecond,
+			f: func(c chan struct{}) error {
+				c <- struct{}{}
+				return nil
+			},
+		},
+		{
+			name:    "timeout exceeded",
+			timeout: 1 * time.Millisecond,
+			f: func(c chan struct{}) error {
+				return nil
+			},
+			err: context.DeadlineExceeded,
+		},
+		{
+			name:    "error before timeout exceeded",
+			timeout: 1 * time.Millisecond,
+			f: func(c chan struct{}) error {
+				return randomError
+			},
+			err: randomError,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			err := RetryFor(c.timeout, c.f)
+			assert.Equal(tt, c.err, err)
+		})
+	}
 }
