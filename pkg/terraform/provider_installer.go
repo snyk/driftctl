792d09cd0b8447cb8607d14262623d08f3f539cc
Elie <elie.charra@cloudskiff.com>
2021-02-08 16:42:35 +0100

    Make provider install and download independent from aws

--- pkg/terraform/provider_installer.go
+++ pkg/terraform/provider_installer.go
@@ -10,43 +10,43 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const awsProviderName = "terraform-provider-aws_v3.19.0_x5"
-
 type HomeDirInterface interface {
 	Dir() (string, error)
 }
 
 type ProviderInstaller struct {
 	downloader ProviderDownloaderInterface
+	config     ProviderConfig
 	homeDir    string
 }
 
-func NewProviderInstaller() (*ProviderInstaller, error) {
+func NewProviderInstaller(config ProviderConfig) (*ProviderInstaller, error) {
 	homedir, err := homedir.Dir()
 	if err != nil {
 		homedir = ""
 	}
 	return &ProviderInstaller{
 		NewProviderDownloader(),
+		config,
 		homedir,
 	}, nil
 }
 
-func (p *ProviderInstaller) GetAws() (string, error) {
+func (p *ProviderInstaller) Install() (string, error) {
 	if p.homeDir == "" {
 		p.homeDir = os.TempDir()
 	}
 	providerDir := path.Join(p.homeDir, fmt.Sprintf("/.driftctl/plugins/%s_%s/", runtime.GOOS, runtime.GOARCH))
-	providerPath := path.Join(providerDir, awsProviderName)
+	providerPath := path.Join(providerDir, p.config.GetBinaryName())
 
 	info, err := os.Stat(providerPath)
 	if err != nil && os.IsNotExist(err) {
 		logrus.WithFields(logrus.Fields{
 			"path": providerPath,
-		}).Debug("AWS provider not found, downloading ...")
-		fmt.Printf("Downloading AWS terraform provider: %s\n", awsProviderName)
+		}).Debug("provider not found, downloading ...")
+		fmt.Printf("Downloading terraform provider: %s\n", p.config.Key)
 		err := p.downloader.Download(
-			p.downloader.GetProviderUrl(AWS, "3.19.0"),
+			p.config.GetDownloadUrl(),
 			providerDir,
 		)
 		if err != nil {
@@ -62,7 +62,7 @@ func (p *ProviderInstaller) GetAws() (string, error) {
 	if info != nil {
 		logrus.WithFields(logrus.Fields{
 			"path": providerPath,
-		}).Debug("Found existing aws provider")
+		}).Debug("Found existing provider")
 	}
 
 	return providerPath, nil
