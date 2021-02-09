5be35dbb0f9cd0928b0ee491addd62c473d6998f
Martin Guibert <martin@cloudskiff.com>
2021-02-09 16:54:50 +0100

    add polling for sns acc tests

--- pkg/resource/aws/aws_sns_topic_policy_test.go
+++ pkg/resource/aws/aws_sns_topic_policy_test.go
@@ -2,8 +2,12 @@ package aws_test
 
 import (
 	"testing"
+	"time"
 
+	"github.com/cloudskiff/driftctl/pkg/remote/aws/repository"
 	"github.com/cloudskiff/driftctl/test/acceptance"
+	"github.com/cloudskiff/driftctl/test/acceptance/awsutils"
+	"github.com/sirupsen/logrus"
 )
 
 func TestAcc_AwsSNSTopicPolicy(t *testing.T) {
@@ -14,6 +18,23 @@ func TestAcc_AwsSNSTopicPolicy(t *testing.T) {
 			{
 				Env: map[string]string{
 					"AWS_REGION": "us-east-1",
+				},
+				PreExec: func() {
+					err := acceptance.RetryFor(60*time.Second, func(doneCh chan struct{}) error {
+						client := repository.NewSNSClient(awsutils.Session())
+						topics, err := client.ListAllTopics()
+						if err != nil {
+							logrus.Warnf("Cannot list topics: %+v", err)
+							return err
+						}
+						if len(topics) == 3 {
+							doneCh <- struct{}{}
+						}
+						return nil
+					})
+					if err != nil {
+						t.Fatal("Timeout while fetching SNS TOPIC")
+					}
 				},
 				Check: func(result *acceptance.ScanResult, stdout string, err error) {
 					if err != nil {
