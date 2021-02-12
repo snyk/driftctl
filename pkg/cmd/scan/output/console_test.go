b63533b2dfb4a1649ba032ae13e47cf35be6c8eb
Louis TOUSSAINT <louistoussaint98@gmail.com>
2021-02-12 22:33:36 +0100

    Issue 226: Update console output to display policy if an access denied is encountered

--- pkg/cmd/scan/output/console_test.go
+++ pkg/cmd/scan/output/console_test.go
@@ -55,6 +55,12 @@ func TestConsole_Write(t *testing.T) {
 			args:       args{analysis: fakeAnalysisWithComputedFields()},
 			wantErr:    false,
 		},
+		{
+			name:       "test console output with enumeration alerts",
+			goldenfile: "output_access_denied_alert.txt",
+			args:       args{analysis: fakeAnalysisWithEnumerationError()},
+			wantErr:    false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
