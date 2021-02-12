399a2c3d58283c0c2e0828149c75477b7e8265b9
Louis TOUSSAINT <louistoussaint98@gmail.com>
2021-02-12 22:27:12 +0100

    Issue 226: Modify Alert type to interface and add serializable Alert struct

--- pkg/alerter/alert.go
+++ pkg/alerter/alert.go
@@ -1,8 +1,53 @@
 package alerter
 
+import "encoding/json"
+
 type Alerts map[string][]Alert
 
-type Alert struct {
-	Message              string `json:"message"`
-	ShouldIgnoreResource bool   `json:"-"`
+type Alert interface {
+	Message() string
+	ShouldIgnoreResource() bool
+}
+
+type FakeAlert struct {
+	Msg            string
+	IgnoreResource bool
+}
+
+func (f *FakeAlert) Message() string {
+	return f.Msg
+}
+
+func (f *FakeAlert) ShouldIgnoreResource() bool {
+	return f.IgnoreResource
+}
+
+type SerializableAlert struct {
+	Alert
+}
+
+type SerializedAlert struct {
+	Msg string `json:"message"`
+}
+
+func (u *SerializedAlert) Message() string {
+	return u.Msg
+}
+
+func (u *SerializedAlert) ShouldIgnoreResource() bool {
+	return false
+}
+
+func (s *SerializableAlert) UnmarshalJSON(bytes []byte) error {
+	var res SerializedAlert
+
+	if err := json.Unmarshal(bytes, &res); err != nil {
+		return err
+	}
+	s.Alert = &res
+	return nil
+}
+
+func (s *SerializableAlert) MarshalJSON() ([]byte, error) {
+	return json.Marshal(SerializedAlert{Msg: s.Message()})
 }
