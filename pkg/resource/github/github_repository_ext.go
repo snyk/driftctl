68d0dbc5f571c32ad539a4d4846fa6aeb4135f2b
Elie <elie.charra@cloudskiff.com>
2021-02-18 16:29:22 +0100

    Remove fullname display for GH repos

--- pkg/resource/github/github_repository_ext.go
+++ pkg/resource/github/github_repository_ext.go
@@ -2,13 +2,6 @@ package github
 
 import "github.com/cloudskiff/driftctl/pkg/resource"
 
-func (r *GithubRepository) String() string {
-	if r.FullName == nil {
-		return r.Id
-	}
-	return *r.FullName
-}
-
 func (r *GithubRepository) NormalizeForState() (resource.Resource, error) {
 	if r.Topics != nil && len(*r.Topics) == 0 {
 		r.Topics = nil
