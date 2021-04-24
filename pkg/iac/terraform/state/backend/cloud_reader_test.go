b555b8b58a0eddb9b478a3219d7ae96804cf1ac6
Rémi Doreau <remi.d45@gmail.com>
2021-04-24 23:32:11 +0200

    feat(state): add cloud reader

---
+++ pkg/iac/terraform/state/backend/cloud_reader_test.go
@@ -0,0 +1,88 @@
+package backend
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCloudReader(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	type args struct {
+		workspaceId string
+		options     *Options
+	}
+	tests := []struct {
+		name      string
+		args      args
+		url       string
+		wantURL   string
+		wantErr   error
+		responder httpmock.Responder
+	}{
+		{
+			name: "Should fetch URL with auth header",
+			args: args{
+				workspaceId: "workspaceId",
+				options: &Options{
+					Headers: map[string]string{
+						"Authorization": "Bearer TOKEN",
+					},
+				},
+			},
+			url:       "https://app.terraform.io/api/v2/workspaces/workspaceId/current-state-version",
+			wantURL:   "https://archivist.terraform.io/v1/object/test",
+			wantErr:   nil,
+			responder: httpmock.NewBytesResponder(http.StatusOK, []byte(`{"data":{"attributes":{"hosted-state-download-url":"URL"}}}`)),
+		},
+		{
+			name: "Should fail with wrong workspaceId",
+			args: args{
+				workspaceId: "wrong_workspaceId",
+				options: &Options{
+					Headers: map[string]string{
+						"Authorization": "Bearer TOKEN",
+					},
+				},
+			},
+			url:       "https://app.terraform.io/api/v2/workspaces/wrong_workspaceId/current-state-version",
+			wantURL:   "",
+			wantErr:   errors.New("Error reading state from Terraform Cloud/Enterprise workspace: wrong workspace id"),
+			responder: httpmock.NewBytesResponder(http.StatusNotFound, []byte{}),
+		},
+		{
+			name: "Should fail with bad authentication token",
+			args: args{
+				workspaceId: "workspaceId",
+				options: &Options{
+					Headers: map[string]string{
+						"Authorization": "Bearer WRONG_TOKEN",
+					},
+				},
+			},
+			url:       "https://app.terraform.io/api/v2/workspaces/workspaceId/current-state-version",
+			wantURL:   "",
+			wantErr:   errors.New("Error reading state from Terraform Cloud/Enterprise workspace: bad authentication token"),
+			responder: httpmock.NewBytesResponder(http.StatusUnauthorized, []byte{}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			httpmock.Reset()
+			httpmock.RegisterResponder("GET", tt.url, tt.responder)
+			got, err := NewHTTPReader(tt.args.workspaceId, tt.args.options)
+			if tt.wantErr != nil {
+				assert.EqualError(t, err, tt.wantErr.Error())
+				return
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.NotNil(t, got)
+			assert.Equal(t, tt.wantURL, got.url)
+		})
+	}
+}
