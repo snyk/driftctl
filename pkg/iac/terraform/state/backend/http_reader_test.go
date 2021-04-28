8116ac4ccfbc1da412bb9dafb613a84037c702f2
sundowndev <raphael@crvx.fr>
2021-04-28 17:06:17 +0200

    test: mock http client

--- pkg/iac/terraform/state/backend/http_reader_test.go
+++ pkg/iac/terraform/state/backend/http_reader_test.go
@@ -3,10 +3,14 @@ package backend
 import (
 	"errors"
 	"io"
+	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+
+	"github.com/cloudskiff/driftctl/mocks"
 )
 
 func TestNewHTTPReader(t *testing.T) {
@@ -15,10 +19,10 @@ func TestNewHTTPReader(t *testing.T) {
 		options *Options
 	}
 	tests := []struct {
-		name    string
-		args    args
-		wantURL string
-		wantErr error
+		name       string
+		args       args
+		wantErr    error
+		httpClient func() HttpClient
 	}{
 		{
 			name: "Should fail with wrong URL",
@@ -28,37 +32,69 @@ func TestNewHTTPReader(t *testing.T) {
 					Headers: map[string]string{},
 				},
 			},
-			wantURL: "",
 			wantErr: errors.New("Get \"wrong_url\": unsupported protocol scheme \"\""),
+			httpClient: func() HttpClient {
+				return &http.Client{}
+			},
 		},
 		{
 			name: "Should fetch URL with auth header",
 			args: args{
-				url: "https://raw.githubusercontent.com/cloudskiff/driftctl/main/.dockerignore",
+				url: "https://wrong.url/cloudskiff/driftctl/main/terraform.tfstate",
 				options: &Options{
 					Headers: map[string]string{
 						"Authorization": "Basic Test",
 					},
 				},
 			},
-			wantURL: "https://raw.githubusercontent.com/cloudskiff/driftctl/main/.dockerignore",
 			wantErr: nil,
+			httpClient: func() HttpClient {
+				m := &mocks.HttpClient{}
+
+				req, _ := http.NewRequest(http.MethodGet, "https://wrong.url/cloudskiff/driftctl/main/terraform.tfstate", nil)
+
+				req.Header.Add("Authorization", "Basic Test")
+
+				bodyReader := strings.NewReader("test")
+				bodyReadCloser := io.NopCloser(bodyReader)
+
+				m.On("Do", req).Return(&http.Response{
+					StatusCode: 200,
+					Body:       bodyReadCloser,
+				}, nil)
+
+				return m
+			},
 		},
 		{
 			name: "Should fail with bad status code",
 			args: args{
-				url: "https://raw.githubusercontent.com/cloudskiff/driftctl-test/main/.dockerignore",
+				url: "https://wrong.url/cloudskiff/driftctl/main/terraform.tfstate",
 				options: &Options{
 					Headers: map[string]string{},
 				},
 			},
-			wantURL: "https://raw.githubusercontent.com/cloudskiff/driftctl-badprojecturl",
 			wantErr: errors.New("error requesting HTTP(s) backend state: status code: 404"),
+			httpClient: func() HttpClient {
+				m := &mocks.HttpClient{}
+
+				req, _ := http.NewRequest(http.MethodGet, "https://wrong.url/cloudskiff/driftctl/main/terraform.tfstate", nil)
+
+				bodyReader := strings.NewReader("test")
+				bodyReadCloser := io.NopCloser(bodyReader)
+
+				m.On("Do", req).Return(&http.Response{
+					StatusCode: 404,
+					Body:       bodyReadCloser,
+				}, nil)
+
+				return m
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewHTTPReader(tt.args.url, tt.args.options)
+			got, err := NewHTTPReader(tt.httpClient(), tt.args.url, tt.args.options)
 			if tt.wantErr != nil {
 				assert.EqualError(t, err, tt.wantErr.Error())
 				return
@@ -66,7 +102,6 @@ func TestNewHTTPReader(t *testing.T) {
 				assert.NoError(t, err)
 			}
 			assert.NotNil(t, got)
-			assert.Equal(t, tt.wantURL, got.url)
 		})
 	}
 }
