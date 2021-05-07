c76702bbb1c3213b4a70777ea0ca841815172eaa
sundowndev <raphael@crvx.fr>
2021-05-07 19:08:17 +0200

    fix: do not ignore default policy attachments

--- pkg/middlewares/aws_defaults.go
+++ pkg/middlewares/aws_defaults.go
@@ -36,39 +36,6 @@ func (m AwsDefaults) awsIamRoleDefaults(remoteResources []resource.Resource) []r
 	return resourcesToIgnore
 }
 
-func (m AwsDefaults) awsIamPolicyAttachmentDefaults(remoteResources []resource.Resource) []resource.Resource {
-	resourcesToIgnore := make([]resource.Resource, 0)
-
-	for _, remoteResource := range remoteResources {
-		// Ignore all resources other than iam policy attachment
-		if remoteResource.TerraformType() != aws.AwsIamPolicyAttachmentResourceType {
-			continue
-		}
-
-		defaultRolesCount := 0
-		for _, roleId := range *remoteResource.(*aws.AwsIamPolicyAttachment).Roles {
-			var role *aws.AwsIamRole
-			for _, res := range remoteResources {
-				if res.TerraformType() == aws.AwsIamRoleResourceType && res.TerraformId() == roleId {
-					role = res.(*aws.AwsIamRole)
-					break
-				}
-			}
-
-			if match := strings.HasPrefix(*role.Path, defaultIamRolePathPrefix); match {
-				defaultRolesCount++
-			}
-		}
-
-		// Check if all of the policy's roles are default AWS roles
-		if defaultRolesCount == len(*remoteResource.(*aws.AwsIamPolicyAttachment).Roles) {
-			resourcesToIgnore = append(resourcesToIgnore, remoteResource)
-		}
-	}
-
-	return resourcesToIgnore
-}
-
 func (m AwsDefaults) awsIamRolePolicyDefaults(remoteResources []resource.Resource) []resource.Resource {
 	resourcesToIgnore := make([]resource.Resource, 0)
 
@@ -100,7 +67,6 @@ func (m AwsDefaults) Execute(remoteResources, resourcesFromState *[]resource.Res
 	resourcesToIgnore := make([]resource.Resource, 0)
 
 	resourcesToIgnore = append(resourcesToIgnore, m.awsIamRoleDefaults(*remoteResources)...)
-	resourcesToIgnore = append(resourcesToIgnore, m.awsIamPolicyAttachmentDefaults(*remoteResources)...)
 	resourcesToIgnore = append(resourcesToIgnore, m.awsIamRolePolicyDefaults(*remoteResources)...)
 
 	for _, res := range *remoteResources {
