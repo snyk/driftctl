fe3d0a14eaa420f2f5be7f103b21e4e94463e744
sundowndev <raphael@crvx.fr>
2021-05-14 11:46:01 +0200

    docs: improve gen-driftignore help message

--- pkg/cmd/gen_driftignore.go
+++ pkg/cmd/gen_driftignore.go
@@ -13,11 +13,10 @@ func NewGenDriftIgnoreCmd() *cobra.Command {
 	opts := &analyser.GenDriftIgnoreOptions{}
 
 	cmd := &cobra.Command{
-		Use:     "gen-driftignore",
-		Short:   "Generate a .driftignore file based on your scan result",
-		Long:    "This command will generate a new .driftignore file containing your current drifts and send output to /dev/stdout",
-		Example: "driftctl scan -o json://stdout | driftctl gen-driftignore",
-		Args:    cobra.NoArgs,
+		Use:   "gen-driftignore",
+		Short: "Generate a .driftignore file based on your scan result",
+		Long:  "This command will generate a new .driftignore file containing your current drifts and send output to /dev/stdout\n\nExample: driftctl scan -o json://stdout | driftctl gen-driftignore > .driftignore",
+		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			_, list, err := genDriftIgnore(opts)
 			if err != nil {
@@ -35,7 +34,7 @@ func NewGenDriftIgnoreCmd() *cobra.Command {
 	fl.BoolVar(&opts.ExcludeUnmanaged, "exclude-unmanaged", false, "Exclude resources not managed by IaC")
 	fl.BoolVar(&opts.ExcludeDeleted, "exclude-missing", false, "Exclude missing resources")
 	fl.BoolVar(&opts.ExcludeDrifted, "exclude-changed", false, "Exclude resources that changed on cloud provider")
-	fl.StringVarP(&opts.InputPath, "from", "f", "/dev/stdin", "Input where the JSON should be parsed")
+	fl.StringVarP(&opts.InputPath, "from", "f", "/dev/stdin", "Input where the JSON should be parsed from")
 
 	return cmd
 }
