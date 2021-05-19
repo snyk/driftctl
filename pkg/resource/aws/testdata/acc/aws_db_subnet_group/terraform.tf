150abdc259da9c031b4cb6fee858e8ad10ae59c5
William Beuil <william.beuil@gmail.com>
2021-05-19 17:48:47 +0200

    Fix terraform file for db_subnet_group

--- pkg/resource/aws/testdata/acc/aws_db_subnet_group/terraform.tf
+++ pkg/resource/aws/testdata/acc/aws_db_subnet_group/terraform.tf
@@ -15,21 +15,25 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "subnet1" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "10.1.0.0/24"
+    availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "subnet2" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "10.1.1.0/24"
+    availability_zone = "us-east-1b"
 }
 
 resource "aws_subnet" "subnet3" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "10.1.2.0/24"
+    availability_zone = "us-east-1c"
 }
 
 resource "aws_subnet" "subnet4" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "10.1.3.0/24"
+    availability_zone = "us-east-1a"
 }
 
 resource "aws_db_subnet_group" "foo" {
