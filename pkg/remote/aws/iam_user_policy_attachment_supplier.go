b35eac7bc6602b7b92b56854f87a90a604100b61
Louis TOUSSAINT <louistoussaint98@gmail.com>
2021-05-19 17:05:41 +0200

    Issue 165: Delete hidden dependency for iam_user_policy_attachment_supplier

--- pkg/remote/aws/iam_user_policy_attachment_supplier.go
+++ pkg/remote/aws/iam_user_policy_attachment_supplier.go
@@ -2,6 +2,9 @@ package aws
 
 import (
 	"fmt"
+	"github.com/cloudskiff/driftctl/pkg/remote/aws/repository"
+	"github.com/cloudskiff/driftctl/pkg/remote/deserializer"
+	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
 
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/iam/iamiface"
@@ -19,7 +22,7 @@ import (
 type IamUserPolicyAttachmentSupplier struct {
 	reader       terraform.ResourceReader
 	deserializer *resource.Deserializer
-	client       iamiface.IAMAPI
+	client       repository.IAMRepository
 	runner       *terraform.ParallelResourceReader
 }
 
@@ -27,32 +30,26 @@ func NewIamUserPolicyAttachmentSupplier(provider *AWSTerraformProvider, deserial
 	return &IamUserPolicyAttachmentSupplier{
 		provider,
 		deserializer,
-		iam.New(provider.session),
+		repository.NewIAMClient(provider.session),
 		terraform.NewParallelResourceReader(provider.Runner().SubRunner()),
 	}
 }
 
 func (s *IamUserPolicyAttachmentSupplier) Resources() ([]resource.Resource, error) {
-	users, err := listIamUsers(s.client, resourceaws.AwsIamUserPolicyAttachmentResourceType)
+	users, err := s.client.ListAllUsers()
 	if err != nil {
-		return nil, err
+		return nil, remoteerror.NewResourceEnumerationErrorWithType(err, resourceaws.AwsIamUserPolicyAttachmentResourceType, resourceaws.AwsIamUserResourceType)
+	}
+	policyAttachments, err := s.client.ListAllUserPolicyAttachments(users)
+	if err != nil {
+		return nil, remoteerror.NewResourceEnumerationError(err, resourceaws.AwsIamUserPolicyAttachmentResourceType)
 	}
 	results := make([]cty.Value, 0)
-	if len(users) > 0 {
-		attachedPolicies := make([]*attachedUserPolicy, 0)
-		for _, user := range users {
-			userName := *user.UserName
-			policyAttachmentList, err := listIamUserPoliciesAttachment(userName, s.client)
-			if err != nil {
-				return nil, remoteerror.NewResourceEnumerationError(err, resourceaws.AwsIamUserPolicyAttachmentResourceType)
-			}
-			attachedPolicies = append(attachedPolicies, policyAttachmentList...)
-		}
-
-		for _, attachedPolicy := range attachedPolicies {
+	if len(policyAttachments) > 0 {
+		for _, attachedPolicy := range policyAttachments {
 			attached := *attachedPolicy
 			s.runner.Run(func() (cty.Value, error) {
-				return s.readRes(attached)
+				return s.readUserPolicyAttachment(&attached)
 			})
 		}
 		results, err = s.runner.Wait()
@@ -64,13 +61,13 @@ func (s *IamUserPolicyAttachmentSupplier) Resources() ([]resource.Resource, erro
 	return s.deserializer.Deserialize(resourceaws.AwsIamUserPolicyAttachmentResourceType, results)
 }
 
-func (s *IamUserPolicyAttachmentSupplier) readRes(attachedPol attachedUserPolicy) (cty.Value, error) {
+func (s *IamUserPolicyAttachmentSupplier) readUserPolicyAttachment(attachedPol *repository.AttachedUserPolicy) (cty.Value, error) {
 	res, err := s.reader.ReadResource(
 		terraform.ReadResourceArgs{
 			Ty: resourceaws.AwsIamUserPolicyAttachmentResourceType,
 			ID: fmt.Sprintf("%s-%s", *attachedPol.PolicyName, attachedPol.Username),
 			Attributes: map[string]string{
-				"user":       attachedPol.Username,
+				"user":       attachedPol.UserName,
 				"policy_arn": *attachedPol.PolicyArn,
 			},
 		},
@@ -81,29 +78,4 @@ func (s *IamUserPolicyAttachmentSupplier) readRes(attachedPol attachedUserPolicy
 		return cty.NilVal, err
 	}
 	return *res, nil
-}
-
-func listIamUserPoliciesAttachment(username string, client iamiface.IAMAPI) ([]*attachedUserPolicy, error) {
-	var attachedUserPolicies []*attachedUserPolicy
-	input := &iam.ListAttachedUserPoliciesInput{
-		UserName: &username,
-	}
-	err := client.ListAttachedUserPoliciesPages(input, func(res *iam.ListAttachedUserPoliciesOutput, lastPage bool) bool {
-		for _, policy := range res.AttachedPolicies {
-			attachedUserPolicies = append(attachedUserPolicies, &attachedUserPolicy{
-				AttachedPolicy: *policy,
-				Username:       username,
-			})
-		}
-		return !lastPage
-	})
-	if err != nil {
-		return nil, err
-	}
-	return attachedUserPolicies, nil
-}
-
-type attachedUserPolicy struct {
-	iam.AttachedPolicy
-	Username string
 }
