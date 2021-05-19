f81cfb9eaf29cfec31d097939f3f4beddf7f9500
Louis TOUSSAINT <louistoussaint98@gmail.com>
2021-05-19 17:12:08 +0200

    Issue 165: Delete hidden dependency for iam_access_key_supplier

--- pkg/remote/aws/iam_access_key_supplier.go
+++ pkg/remote/aws/iam_access_key_supplier.go
@@ -2,9 +2,10 @@ package aws
 
 import (
 	"github.com/aws/aws-sdk-go/service/iam"
-	"github.com/aws/aws-sdk-go/service/iam/iamiface"
-	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
 
+	"github.com/cloudskiff/driftctl/pkg/remote/aws/repository"
+	"github.com/cloudskiff/driftctl/pkg/remote/deserializer"
+	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
 	"github.com/cloudskiff/driftctl/pkg/resource"
 	resourceaws "github.com/cloudskiff/driftctl/pkg/resource/aws"
 
@@ -17,7 +18,7 @@ import (
 type IamAccessKeySupplier struct {
 	reader       terraform.ResourceReader
 	deserializer *resource.Deserializer
-	client       iamiface.IAMAPI
+	client       repository.IAMRepository
 	runner       *terraform.ParallelResourceReader
 }
 
@@ -25,22 +26,26 @@ func NewIamAccessKeySupplier(provider *AWSTerraformProvider, deserializer *resou
 	return &IamAccessKeySupplier{
 		provider,
 		deserializer,
-		iam.New(provider.session),
+		repository.NewIAMClient(provider.session),
 		terraform.NewParallelResourceReader(provider.Runner().SubRunner()),
 	}
 }
 
 func (s *IamAccessKeySupplier) Resources() ([]resource.Resource, error) {
-	keys, err := listIamAccessKeys(s.client)
+	users, err := s.client.ListAllUsers()
 	if err != nil {
-		return nil, err
+		return nil, remoteerror.NewResourceEnumerationErrorWithType(err, resourceaws.AwsIamAccessKeyResourceType, resourceaws.AwsIamUserResourceType)
+	}
+	keys, err := s.client.ListAllAccessKeys(users)
+	if err != nil {
+		return nil, remoteerror.NewResourceEnumerationError(err, resourceaws.AwsIamAccessKeyResourceType)
 	}
 	results := make([]cty.Value, 0)
 	if len(keys) > 0 {
 		for _, key := range keys {
 			k := *key
 			s.runner.Run(func() (cty.Value, error) {
-				return s.readRes(&k)
+				return s.readAccessKey(&k)
 			})
 		}
 		results, err = s.runner.Wait()
@@ -51,7 +56,7 @@ func (s *IamAccessKeySupplier) Resources() ([]resource.Resource, error) {
 	return s.deserializer.Deserialize(resourceaws.AwsIamAccessKeyResourceType, results)
 }
 
-func (s *IamAccessKeySupplier) readRes(key *iam.AccessKeyMetadata) (cty.Value, error) {
+func (s *IamAccessKeySupplier) readAccessKey(key *iam.AccessKeyMetadata) (cty.Value, error) {
 	res, err := s.reader.ReadResource(
 		terraform.ReadResourceArgs{
 			Ty: resourceaws.AwsIamAccessKeyResourceType,
@@ -67,26 +72,4 @@ func (s *IamAccessKeySupplier) readRes(key *iam.AccessKeyMetadata) (cty.Value, e
 	}
 
 	return *res, nil
-}
-
-func listIamAccessKeys(client iamiface.IAMAPI) ([]*iam.AccessKeyMetadata, error) {
-	users, err := listIamUsers(client, resourceaws.AwsIamAccessKeyResourceType)
-	if err != nil {
-		return nil, err
-	}
-	var resources []*iam.AccessKeyMetadata
-	for _, user := range users {
-		input := &iam.ListAccessKeysInput{
-			UserName: user.UserName,
-		}
-		err := client.ListAccessKeysPages(input, func(res *iam.ListAccessKeysOutput, lastPage bool) bool {
-			resources = append(resources, res.AccessKeyMetadata...)
-			return !lastPage
-		})
-		if err != nil {
-			return nil, remoteerror.NewResourceEnumerationError(err, resourceaws.AwsIamAccessKeyResourceType)
-		}
-	}
-
-	return resources, nil
 }
