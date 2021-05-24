af08a560e11e1ac1b31e87b6b8c66f6a309d5476
Louis TOUSSAINT <louistoussaint98@gmail.com>
2021-05-24 17:44:17 +0200

    Issue 543: Fix false positive on aws_db_instance

---
+++ pkg/resource/aws/testdata/acc/aws_db_instance/db_instance.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
