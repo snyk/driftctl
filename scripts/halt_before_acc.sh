66ad9883422f955ce67c01af0ded2787530fbd8e
Martin Guibert <martin@cloudskiff.com>
2021-05-31 17:32:16 +0200

    [RUN ACC] add tags to be able to run acc test on pr

---
+++ scripts/halt_before_acc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+COMMIT_MESSAGE="$(git log --format=oneline -n 1 $CIRCLE_SHA1)"
+
+if [[ $COMMIT_MESSAGE == *"[RUN ACC]"* ]] || [[ $FORCE_ACC_TEST == "true" ]]; then
+  # If user is not member of core team, context is not applie so acc test won't run anyway
+  echo "Running acceptance tests"
+else
+  circleci-agent step halt
+fi
