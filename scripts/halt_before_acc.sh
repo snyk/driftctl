a693499c0d837241d479acb25c13698d2d13f92f
Martin Guibert <martin@cloudskiff.com>
2021-06-01 15:42:51 +0200

    name acc test check step & use parameter as force var env

--- scripts/halt_before_acc.sh
+++ scripts/halt_before_acc.sh
@@ -6,5 +6,6 @@ if [[ $COMMIT_MESSAGE == *"[RUN ACC]"* ]] || [[ $FORCE_ACC_TEST == "true" ]]; th
   # If user is not member of core team, context is not applie so acc test won't run anyway
   echo "Running acceptance tests"
 else
+  echo "Not running acceptance tests"
   circleci-agent step halt
 fi
