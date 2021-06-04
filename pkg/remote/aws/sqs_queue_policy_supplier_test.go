de9d246f98b36ef92c3210ef4821d4a7f10d3474
sundowndev <raphael@crvx.fr>
2021-06-04 16:14:05 +0200

    feat: implement cache in SQS repository

--- pkg/remote/aws/sqs_queue_policy_supplier_test.go
+++ pkg/remote/aws/sqs_queue_policy_supplier_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/cloudskiff/driftctl/pkg/remote/aws/repository"
+	"github.com/cloudskiff/driftctl/pkg/remote/cache"
 	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
 	resourceaws "github.com/cloudskiff/driftctl/pkg/resource/aws"
 	testresource "github.com/cloudskiff/driftctl/test/resource"
@@ -77,7 +78,7 @@ func TestSqsQueuePolicySupplier_Resources(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			supplierLibrary.AddSupplier(NewSqsQueuePolicySupplier(provider, deserializer, repository.NewSQSClient(provider.session)))
+			supplierLibrary.AddSupplier(NewSqsQueuePolicySupplier(provider, deserializer, repository.NewSQSClient(provider.session, cache.New(0))))
 		}
 
 		t.Run(c.test, func(tt *testing.T) {
