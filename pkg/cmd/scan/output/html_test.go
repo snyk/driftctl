067241ed2a91f0d77f4ad030c06667f6806a57e9
sundowndev <raphael@crvx.fr>
2021-06-07 15:21:57 +0200

    refactor: html output

--- pkg/cmd/scan/output/html_test.go
+++ pkg/cmd/scan/output/html_test.go
@@ -5,6 +5,8 @@ import (
 	"path"
 	"testing"
 
+	"github.com/cloudskiff/driftctl/pkg/resource"
+	testresource "github.com/cloudskiff/driftctl/test/resource"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/cloudskiff/driftctl/pkg/analyser"
@@ -65,6 +67,52 @@ func TestHTML_Write(t *testing.T) {
 			}
 
 			assert.Equal(t, string(expected), string(got))
+		})
+	}
+}
+
+func TestHTML_DistinctResourceTypes(t *testing.T) {
+	tests := []struct {
+		name      string
+		resources []resource.Resource
+		value     []string
+	}{
+		{
+			name:      "test empty array",
+			resources: []resource.Resource{},
+			value:     []string{},
+		},
+		{
+			name: "test empty array",
+			resources: []resource.Resource{
+				testresource.FakeResource{
+					Id:   "deleted-id-1",
+					Type: "aws_deleted_resource",
+				},
+				testresource.FakeResource{
+					Id:   "unmanaged-id-1",
+					Type: "aws_unmanaged_resource",
+				},
+				testresource.FakeResource{
+					Id:   "unmanaged-id-2",
+					Type: "aws_unmanaged_resource",
+				},
+				testresource.FakeResource{
+					Id:   "diff-id-1",
+					Type: "aws_diff_resource",
+				},
+				testresource.FakeResource{
+					Id:   "deleted-id-2",
+					Type: "aws_deleted_resource",
+				},
+			},
+			value: []string{"aws_deleted_resource", "aws_unmanaged_resource", "aws_diff_resource"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := distinctResourceTypes(tt.resources)
+			assert.Equal(t, tt.value, got)
 		})
 	}
 }
