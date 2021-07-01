4a8eea249e6c34a94972b2e3233d906d2e2e6fc0
William Beuil <william.beuil@gmail.com>
2021-07-01 12:19:34 +0200

    Split ec2_eip_supplier

---
+++ pkg/remote/aws/ec2_eip_enumerator.go
@@ -0,0 +1,49 @@
+package aws
+
+import (
+	"github.com/cloudskiff/driftctl/pkg/remote/aws/repository"
+	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
+	tf "github.com/cloudskiff/driftctl/pkg/remote/terraform"
+	"github.com/cloudskiff/driftctl/pkg/resource"
+	"github.com/cloudskiff/driftctl/pkg/resource/aws"
+)
+
+type EC2EipEnumerator struct {
+	repository     repository.EC2Repository
+	factory        resource.ResourceFactory
+	providerConfig tf.TerraformProviderConfig
+}
+
+func NewEC2EipEnumerator(repo repository.EC2Repository, factory resource.ResourceFactory, providerConfig tf.TerraformProviderConfig) *EC2EipEnumerator {
+	return &EC2EipEnumerator{
+		repository:     repo,
+		factory:        factory,
+		providerConfig: providerConfig,
+	}
+}
+
+func (e *EC2EipEnumerator) SupportedType() resource.ResourceType {
+	return aws.AwsEipResourceType
+}
+
+func (e *EC2EipEnumerator) Enumerate() ([]resource.Resource, error) {
+	addresses, err := e.repository.ListAllAddresses()
+	if err != nil {
+		return nil, remoteerror.NewResourceEnumerationError(err, string(e.SupportedType()))
+	}
+
+	results := make([]resource.Resource, len(addresses))
+
+	for _, address := range addresses {
+		results = append(
+			results,
+			e.factory.CreateAbstractResource(
+				string(e.SupportedType()),
+				*address.AllocationId,
+				map[string]interface{}{},
+			),
+		)
+	}
+
+	return results, err
+}
