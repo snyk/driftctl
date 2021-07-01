652d84663169c935f80db0c0a2bea8ae6e699451
William Beuil <william.beuil@gmail.com>
2021-07-01 12:09:09 +0200

    Split ec2_ebs_snapshot_supplier

--- pkg/remote/ec2_scanner_test.go
+++ pkg/remote/ec2_scanner_test.go
@@ -114,3 +114,94 @@ func TestEC2EbsVolume(t *testing.T) {
 		})
 	}
 }
+
+func TestEC2EbsSnapshot(t *testing.T) {
+	tests := []struct {
+		test    string
+		dirName string
+		mocks   func(repository *repository.MockEC2Repository)
+		wantErr error
+	}{
+		{
+			test:    "no snapshots",
+			dirName: "aws_ec2_ebs_snapshot_empty",
+			mocks: func(repository *repository.MockEC2Repository) {
+				repository.On("ListAllSnapshots").Return([]*ec2.Snapshot{}, nil)
+			},
+		},
+		{
+			test:    "multiple snapshots",
+			dirName: "aws_ec2_ebs_snapshot_multiple",
+			mocks: func(repository *repository.MockEC2Repository) {
+				repository.On("ListAllSnapshots").Return([]*ec2.Snapshot{
+					{SnapshotId: awssdk.String("snap-0c509a2a880d95a39")},
+					{SnapshotId: awssdk.String("snap-00672558cecd93a61")},
+				}, nil)
+			},
+		},
+		{
+			test:    "cannot list snapshots",
+			dirName: "aws_ec2_ebs_snapshot_list",
+			mocks: func(repository *repository.MockEC2Repository) {
+				repository.On("ListAllSnapshots").Return(nil, awserr.NewRequestFailure(nil, 403, ""))
+			},
+			wantErr: remoteerror.NewResourceEnumerationError(awserr.NewRequestFailure(nil, 403, ""), resourceaws.AwsEbsSnapshotResourceType),
+		},
+	}
+
+	schemaRepository := testresource.InitFakeSchemaRepository("aws", "3.19.0")
+	resourceaws.InitResourcesMetadata(schemaRepository)
+	factory := terraform.NewTerraformResourceFactory(schemaRepository)
+	deserializer := resource.NewDeserializer(factory)
+	alerter := &mocks.AlerterInterface{}
+
+	for _, c := range tests {
+		t.Run(c.test, func(tt *testing.T) {
+			shouldUpdate := c.dirName == *goldenfile.Update
+
+			sess := session.Must(session.NewSessionWithOptions(session.Options{
+				SharedConfigState: session.SharedConfigEnable,
+			}))
+
+			scanOptions := ScannerOptions{Deep: true}
+			providerLibrary := terraform.NewProviderLibrary()
+			remoteLibrary := common.NewRemoteLibrary()
+
+			// Initialize mocks
+			fakeRepo := &repository.MockEC2Repository{}
+			c.mocks(fakeRepo)
+			var repo repository.EC2Repository = fakeRepo
+			providerVersion := "3.19.0"
+			realProvider, err := terraform2.InitTestAwsProvider(providerLibrary, providerVersion)
+			if err != nil {
+				t.Fatal(err)
+			}
+			provider := terraform2.NewFakeTerraformProvider(realProvider)
+			provider.WithResponse(c.dirName)
+
+			// Replace mock by real resources if we are in update mode
+			if shouldUpdate {
+				err := realProvider.Init()
+				if err != nil {
+					t.Fatal(err)
+				}
+				provider.ShouldUpdate()
+				repo = repository.NewEC2Repository(sess, cache.New(0))
+			}
+
+			remoteLibrary.AddEnumerator(aws.NewEC2EbsSnapshotEnumerator(repo, factory, tf.TerraformProviderConfig{
+				Name:         "test",
+				DefaultAlias: "eu-west-3",
+			}))
+			remoteLibrary.AddDetailsFetcher(resourceaws.AwsEbsSnapshotResourceType, common.NewGenericDetailFetcher(resourceaws.AwsEbsSnapshotResourceType, provider, deserializer))
+
+			s := NewScanner(nil, remoteLibrary, alerter, scanOptions)
+			got, err := s.Resources()
+			assert.Equal(tt, err, c.wantErr)
+			if err != nil {
+				return
+			}
+			test.TestAgainstGoldenFile(got, resourceaws.AwsEbsSnapshotResourceType, c.dirName, provider, deserializer, shouldUpdate, tt)
+		})
+	}
+}
