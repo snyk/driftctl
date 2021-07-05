640956f95ce88e627d89d28e78b017454a77483d
William Beuil <william.beuil@gmail.com>
2021-07-05 12:31:11 +0200

    Split route_table_supplier

---
+++ pkg/remote/aws/ec2_default_route_table_details_fetcher.go
@@ -0,0 +1,38 @@
+package aws
+
+import (
+	"github.com/cloudskiff/driftctl/pkg/resource"
+	"github.com/cloudskiff/driftctl/pkg/resource/aws"
+	"github.com/cloudskiff/driftctl/pkg/terraform"
+)
+
+type EC2DefaultRouteTableDetailsFetcher struct {
+	reader       terraform.ResourceReader
+	deserializer *resource.Deserializer
+}
+
+func NewEC2DefaultRouteTableDetailsFetcher(provider terraform.ResourceReader, deserializer *resource.Deserializer) *EC2DefaultRouteTableDetailsFetcher {
+	return &EC2DefaultRouteTableDetailsFetcher{
+		reader:       provider,
+		deserializer: deserializer,
+	}
+}
+
+func (r *EC2DefaultRouteTableDetailsFetcher) ReadDetails(res resource.Resource) (resource.Resource, error) {
+	ctyVal, err := r.reader.ReadResource(terraform.ReadResourceArgs{
+		Ty: aws.AwsDefaultRouteTableResourceType,
+		ID: res.TerraformId(),
+		Attributes: map[string]string{
+			"vpc_id": *res.Attributes().GetString("vpc_id"),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	deserializedRes, err := r.deserializer.DeserializeOne(aws.AwsDefaultRouteTableResourceType, *ctyVal)
+	if err != nil {
+		return nil, err
+	}
+
+	return deserializedRes, nil
+}
---
+++ pkg/remote/aws/ec2_default_route_table_enumerator.go
@@ -0,0 +1,50 @@
+package aws
+
+import (
+	"github.com/cloudskiff/driftctl/pkg/remote/aws/repository"
+	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
+	"github.com/cloudskiff/driftctl/pkg/resource"
+	"github.com/cloudskiff/driftctl/pkg/resource/aws"
+)
+
+type EC2DefaultRouteTableEnumerator struct {
+	repository repository.EC2Repository
+	factory    resource.ResourceFactory
+}
+
+func NewEC2DefaultRouteTableEnumerator(repo repository.EC2Repository, factory resource.ResourceFactory) *EC2DefaultRouteTableEnumerator {
+	return &EC2DefaultRouteTableEnumerator{
+		repository: repo,
+		factory:    factory,
+	}
+}
+
+func (e *EC2DefaultRouteTableEnumerator) SupportedType() resource.ResourceType {
+	return aws.AwsDefaultRouteTableResourceType
+}
+
+func (e *EC2DefaultRouteTableEnumerator) Enumerate() ([]resource.Resource, error) {
+	routeTables, err := e.repository.ListAllRouteTables()
+	if err != nil {
+		return nil, remoteerror.NewResourceEnumerationError(err, string(e.SupportedType()))
+	}
+
+	var results []resource.Resource
+
+	for _, routeTable := range routeTables {
+		if isMainRouteTable(routeTable) {
+			results = append(
+				results,
+				e.factory.CreateAbstractResource(
+					string(e.SupportedType()),
+					*routeTable.RouteTableId,
+					map[string]interface{}{
+						"vpc_id": *routeTable.VpcId,
+					},
+				),
+			)
+		}
+	}
+
+	return results, err
+}
