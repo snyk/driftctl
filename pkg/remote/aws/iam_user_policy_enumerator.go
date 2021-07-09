65a4e82654c2719d65b2fc6a8cfebc12fde93a28
Louis TOUSSAINT <louistoussaint98@gmail.com>
2021-07-09 13:39:50 +0200

    Issue 728: Split iam user policy

---
+++ pkg/remote/aws/iam_user_policy_enumerator.go
@@ -0,0 +1,50 @@
+package aws
+
+import (
+	"github.com/cloudskiff/driftctl/pkg/remote/aws/repository"
+	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
+	"github.com/cloudskiff/driftctl/pkg/resource"
+	"github.com/cloudskiff/driftctl/pkg/resource/aws"
+)
+
+type IamUserPolicyEnumerator struct {
+	repository repository.IAMRepository
+	factory    resource.ResourceFactory
+}
+
+func NewIamUserPolicyEnumerator(repo repository.IAMRepository, factory resource.ResourceFactory) *IamUserPolicyEnumerator {
+	return &IamUserPolicyEnumerator{
+		repository: repo,
+		factory:    factory,
+	}
+}
+
+func (e *IamUserPolicyEnumerator) SupportedType() resource.ResourceType {
+	return aws.AwsIamUserPolicyResourceType
+}
+
+func (e *IamUserPolicyEnumerator) Enumerate() ([]resource.Resource, error) {
+	users, err := e.repository.ListAllUsers()
+	if err != nil {
+		return nil, remoteerror.NewResourceEnumerationErrorWithType(err, string(e.SupportedType()), aws.AwsIamUserResourceType)
+	}
+	userPolicies, err := e.repository.ListAllUserPolicies(users)
+	if err != nil {
+		return nil, remoteerror.NewResourceEnumerationError(err, string(e.SupportedType()))
+	}
+
+	results := make([]resource.Resource, len(userPolicies))
+
+	for _, userPolicy := range userPolicies {
+		results = append(
+			results,
+			e.factory.CreateAbstractResource(
+				string(e.SupportedType()),
+				userPolicy,
+				map[string]interface{}{},
+			),
+		)
+	}
+
+	return results, err
+}
