e27a5564fd81e0ca3eaf6d7e16e0ff2533c91991
sundowndev <raphael@crvx.fr>
2021-07-15 17:36:34 +0200

    refactor: remove usage of flatmap pkg

--- pkg/remote/aws/vpc_security_group_rule_details_fetcher.go
+++ pkg/remote/aws/vpc_security_group_rule_details_fetcher.go
@@ -1,10 +1,11 @@
 package aws
 
 import (
+	"fmt"
+
 	"github.com/cloudskiff/driftctl/pkg/resource"
 	"github.com/cloudskiff/driftctl/pkg/resource/aws"
 	"github.com/cloudskiff/driftctl/pkg/terraform"
-	"github.com/hashicorp/terraform/flatmap"
 )
 
 type VPCSecurityGroupRuleDetailsFetcher struct {
@@ -20,43 +21,43 @@ func NewVPCSecurityGroupRuleDetailsFetcher(provider terraform.ResourceReader, de
 }
 
 func (r *VPCSecurityGroupRuleDetailsFetcher) ReadDetails(res resource.Resource) (resource.Resource, error) {
-	attrs := make(map[string]interface{})
+	attrs := make(map[string]string)
 
 	if v, ok := res.Attributes().Get("type"); ok {
-		attrs["type"] = v
+		attrs["type"] = fmt.Sprintf("%s", v)
 	}
 	if v, ok := res.Attributes().Get("protocol"); ok {
-		attrs["protocol"] = v
+		attrs["protocol"] = fmt.Sprintf("%s", v)
 	}
 	if v, ok := res.Attributes().Get("from_port"); ok {
-		attrs["from_port"] = int(v.(float64))
+		attrs["from_port"] = fmt.Sprintf("%d", int(v.(float64)))
 	}
 	if v, ok := res.Attributes().Get("to_port"); ok {
-		attrs["to_port"] = int(v.(float64))
+		attrs["to_port"] = fmt.Sprintf("%d", int(v.(float64)))
 	}
 	if v, ok := res.Attributes().Get("security_group_id"); ok {
-		attrs["security_group_id"] = v
+		attrs["security_group_id"] = fmt.Sprintf("%s", v)
 	}
 	if v, ok := res.Attributes().Get("self"); ok {
-		attrs["self"] = v
+		attrs["self"] = fmt.Sprintf("%t", v)
 	}
 	if v, ok := res.Attributes().Get("cidr_blocks"); ok {
-		attrs["cidr_blocks"] = v
+		attrs["cidr_blocks"] = fmt.Sprintf("%s", v)
 	}
 	if v, ok := res.Attributes().Get("ipv6_cidr_blocks"); ok {
-		attrs["ipv6_cidr_blocks"] = v
+		attrs["ipv6_cidr_blocks"] = fmt.Sprintf("%s", v)
 	}
 	if v, ok := res.Attributes().Get("prefix_list_ids"); ok {
-		attrs["prefix_list_ids"] = v
+		attrs["prefix_list_ids"] = fmt.Sprintf("%s", v)
 	}
 	if v, ok := res.Attributes().Get("source_security_group_id"); ok {
-		attrs["source_security_group_id"] = v
+		attrs["source_security_group_id"] = fmt.Sprintf("%s", v)
 	}
 
 	ctyVal, err := r.reader.ReadResource(terraform.ReadResourceArgs{
 		Ty:         aws.AwsSecurityGroupRuleResourceType,
 		ID:         res.TerraformId(),
-		Attributes: flatmap.Flatten(attrs),
+		Attributes: attrs,
 	})
 	if err != nil {
 		return nil, err
