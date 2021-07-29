82b8c1c80bdb051f86ec6bad301fd4bd55516165
Elie <elie.charra@cloudskiff.com>
2021-07-29 11:46:31 +0200

    Add resource source for terraform state

--- pkg/iac/config/config.go
+++ pkg/iac/config/config.go
@@ -1,7 +1,23 @@
 package config
 
+import "fmt"
+
 type SupplierConfig struct {
 	Key     string
 	Backend string
 	Path    string
+}
+
+func (c *SupplierConfig) String() string {
+	str := c.Key
+	if c.Backend != "" {
+		str += fmt.Sprintf("+%s", c.Backend)
+	}
+	if str != "" {
+		str += "://"
+	}
+	if c.Path != "" {
+		str += c.Path
+	}
+	return str
 }
