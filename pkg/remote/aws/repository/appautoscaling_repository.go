9d37413f051b88799158c601ea0cde8f3e897a55
sundowndev <raphael@crvx.fr>
2021-09-22 14:55:39 +0200

    refactor: simplify code

--- pkg/remote/aws/repository/appautoscaling_repository.go
+++ pkg/remote/aws/repository/appautoscaling_repository.go
@@ -12,8 +12,6 @@ import (
 type AppAutoScalingRepository interface {
 	ServiceNamespaceValues() []string
 	DescribeScalableTargets(string) ([]*applicationautoscaling.ScalableTarget, error)
-	DescribeScalingPolicies(string) ([]*applicationautoscaling.ScalingPolicy, error)
-	DescribeScheduledActions(string) ([]*applicationautoscaling.ScheduledAction, error)
 }
 
 type appAutoScalingRepository struct {
@@ -48,40 +46,4 @@ func (r *appAutoScalingRepository) DescribeScalableTargets(namespace string) ([]
 
 	r.cache.Put(cacheKey, result.ScalableTargets)
 	return result.ScalableTargets, nil
-}
-
-func (r *appAutoScalingRepository) DescribeScalingPolicies(namespace string) ([]*applicationautoscaling.ScalingPolicy, error) {
-	cacheKey := fmt.Sprintf("appAutoScalingDescribeScalingPolicies_%s", namespace)
-	if v := r.cache.Get(cacheKey); v != nil {
-		return v.([]*applicationautoscaling.ScalingPolicy), nil
-	}
-
-	input := &applicationautoscaling.DescribeScalingPoliciesInput{
-		ServiceNamespace: &namespace,
-	}
-	result, err := r.client.DescribeScalingPolicies(input)
-	if err != nil {
-		return nil, err
-	}
-
-	r.cache.Put(cacheKey, result.ScalingPolicies)
-	return result.ScalingPolicies, nil
-}
-
-func (r *appAutoScalingRepository) DescribeScheduledActions(namespace string) ([]*applicationautoscaling.ScheduledAction, error) {
-	cacheKey := fmt.Sprintf("appAutoScalingDescribeScheduledActions_%s", namespace)
-	if v := r.cache.Get(cacheKey); v != nil {
-		return v.([]*applicationautoscaling.ScheduledAction), nil
-	}
-
-	input := &applicationautoscaling.DescribeScheduledActionsInput{
-		ServiceNamespace: &namespace,
-	}
-	result, err := r.client.DescribeScheduledActions(input)
-	if err != nil {
-		return nil, err
-	}
-
-	r.cache.Put(cacheKey, result.ScheduledActions)
-	return result.ScheduledActions, nil
 }
