6529faf0744a4692b752873c91fbcafa48c6de52
sundowndev <raphael@crvx.fr>
2021-10-01 16:52:46 +0200

    refactor: azurerm_virtual_network resource metadata

--- pkg/resource/azurerm/azurerm_virtual_network.go
+++ pkg/resource/azurerm/azurerm_virtual_network.go
@@ -1,3 +1,18 @@
 package azurerm
 
+import (
+	"github.com/cloudskiff/driftctl/pkg/resource"
+)
+
 const AzureVirtualNetworkResourceType = "azurerm_virtual_network"
+
+func initAzureVirtualNetworkMetaData(resourceSchemaRepository resource.SchemaRepositoryInterface) {
+	resourceSchemaRepository.SetHumanReadableAttributesFunc(AzureVirtualNetworkResourceType, func(res *resource.Resource) map[string]string {
+		attrs := make(map[string]string)
+
+		if v := res.Attributes().GetString("name"); v != nil && *v != "" {
+			attrs["Name"] = *v
+		}
+		return attrs
+	})
+}
