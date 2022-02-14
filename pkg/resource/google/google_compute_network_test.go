ee5118ab131977bba2bf55158cd0fc522a175479
sundowndev <raphael@crvx.fr>
2022-02-14 18:07:15 +0400

    refactor(test): add common exponential backoff function

--- pkg/resource/google/google_compute_network_test.go
+++ pkg/resource/google/google_compute_network_test.go
@@ -20,15 +20,7 @@ func TestAcc_Google_ComputeNetwork(t *testing.T) {
 		Checks: []acceptance.AccCheck{
 			{
 				// New resources are not visible immediately through GCP API after an apply operation.
-				// Logic below retries driftctl scan using a back-off strategy of retrying 'n' times
-				// and doubling the amount of time waited after each one.
-				ShouldRetry: func(result *test.ScanResult, retryDuration time.Duration, retryCount uint8) bool {
-					if result.IsSync() || retryDuration > 15*time.Minute {
-						return false
-					}
-					time.Sleep((2 * time.Duration(retryCount)) * time.Minute)
-					return true
-				},
+				ShouldRetry: acceptance.LinearBackoff(15 * time.Minute),
 				Check: func(result *test.ScanResult, stdout string, err error) {
 					if err != nil {
 						t.Fatal(err)
