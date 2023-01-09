edda766bc74528bedf7b5b21c6bff576a369a416
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-01-09 23:43:47 +0100

    rm `TestGSEnumerator_Enumerate` existing

--- pkg/iac/terraform/state/enumerator/gs_test.go
+++ pkg/iac/terraform/state/enumerator/gs_test.go
@@ -1,12 +1,9 @@
 package enumerator
 
 import (
-	"net/http"
-	"reflect"
 	"testing"
 
 	"github.com/snyk/driftctl/pkg/iac/config"
-	googletest "github.com/snyk/driftctl/test/google"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -55,62 +52,6 @@ func TestGSEnumerator_NewGSEnumerator_HasCorrectConfig(t *testing.T) {
 			assert.Equal(t, tt.config.Key, got.config.Key)
 			assert.Equal(t, tt.config.Backend, got.config.Backend)
 			assert.Equal(t, tt.config.Path, got.config.Path)
-		})
-	}
-}
-
-func TestGSEnumerator_Enumerate(t *testing.T) {
-	tests := []struct {
-		name        string
-		config      config.SupplierConfig
-		handlerFunc map[string]http.HandlerFunc
-		want        []string
-		err         string
-	}{
-		{
-			name: "should succeed",
-			config: config.SupplierConfig{
-				Path: "bucket-1/*.tfstate",
-			},
-			handlerFunc: map[string]http.HandlerFunc{
-				"/bucket-2/path/to/terraform.tfstate": func(w http.ResponseWriter, r *http.Request) {
-					w.WriteHeader(http.StatusOK)
-					_, _ = w.Write([]byte("Not Found"))
-				},
-			},
-			want: []string{
-				"bucket-1/a/nested/prefix/1/state1.tfstate",
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			client, server, err := googletest.NewFakeStorageServer(tt.handlerFunc)
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer client.Close()
-			defer server.Close()
-
-			gs := &GSEnumerator{
-				config: tt.config,
-				client: *client,
-			}
-
-			assert.NoError(t, err)
-
-			got, err := gs.Enumerate()
-			if err != nil && err.Error() != tt.err {
-				t.Fatalf("Expected error '%s', got '%s'", tt.err, err.Error())
-				return
-			}
-			if tt.err != "" && err == nil {
-				t.Fatalf("Expected error '%s' but got nil", tt.err)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Enumerate() got = %v, want %v", got, tt.want)
-			}
 		})
 	}
 }
