c26d63b6105ade53f3e569a938792cc252c3e160
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-01-09 18:18:11 +0100

    split SupplierConfig and client context assertions

--- pkg/iac/terraform/state/enumerator/gs_test.go
+++ pkg/iac/terraform/state/enumerator/gs_test.go
@@ -11,27 +11,48 @@ func TestGSEnumerator_NewGSEnumerator(t *testing.T) {
 	tests := []struct {
 		name   string
 		config config.SupplierConfig
-		setEnv map[string]string
-		want   string
 	}{
 		{
-			name: "test with no proxy env var",
+			name: "test if required attributes are not nil",
 			config: config.SupplierConfig{
 				Key:     "tfstate",
 				Backend: "gs",
 				Path:    "terraform.tfstate",
 			},
-			setEnv: map[string]string{
-				"AWS_DEFAULT_REGION": "us-east-1",
-			},
-			want: "us-east-1",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := NewGSEnumerator(tt.config)
 			assert.NotNil(t, got)
+			assert.NotNil(t, got.client)
+			assert.NotNil(t, got.config)
+			assert.NotNil(t, got.ctx)
+		})
+	}
+}
+
+func TestGSEnumerator_NewGSEnumerator_HasCorrectConfig(t *testing.T) {
+	tests := []struct {
+		name   string
+		config config.SupplierConfig
+	}{
+		{
+			name: "test whether the supplier config isn't changed",
+			config: config.SupplierConfig{
+				Key:     "tfstate",
+				Backend: "gs",
+				Path:    "terraform.tfstate",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewGSEnumerator(tt.config)
+			assert.NotNil(t, got)
+			assert.Equal(t, tt.config.Key, got.config.Key)
 			assert.Equal(t, tt.config.Backend, got.config.Backend)
+			assert.Equal(t, tt.config.Path, got.config.Path)
 		})
 	}
 }
