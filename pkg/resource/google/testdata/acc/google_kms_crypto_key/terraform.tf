3918f35fd5015127e7fb8fb911f7dea5588f118a
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-01-16 17:55:00 +0100

    rm lifecycle

--- pkg/resource/google/testdata/acc/google_kms_crypto_key/terraform.tf
+++ pkg/resource/google/testdata/acc/google_kms_crypto_key/terraform.tf
@@ -20,8 +20,4 @@ resource "google_kms_crypto_key" "example-key" {
   name            = "crypto-key-example"
   key_ring        = google_kms_key_ring.keyring.id
   rotation_period = "100000s"
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
