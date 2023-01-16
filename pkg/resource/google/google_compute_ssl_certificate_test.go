35660022fb232b2c82a19a69f42ff389c7c502a2
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-01-16 17:42:31 +0100

    Create google_compute_ssl_certificate_test.go

---
+++ pkg/resource/google/google_compute_ssl_certificate_test.go
@@ -0,0 +1,33 @@
+package google_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/snyk/driftctl/test"
+	"github.com/snyk/driftctl/test/acceptance"
+)
+
+func TestAcc_Google_ComputeSslCertificate(t *testing.T) {
+	acceptance.Run(t, acceptance.AccTestCase{
+		TerraformVersion: "0.15.5",
+		Paths:            []string{"./testdata/acc/google_compute_ssl_certificate"},
+		Args: []string{
+			"scan",
+			"--to", "gcp+tf",
+		},
+		Checks: []acceptance.AccCheck{
+			{
+				// New resources are not visible immediately through GCP API after an apply operation.
+				ShouldRetry: acceptance.LinearBackoff(10 * time.Minute),
+				Check: func(result *test.ScanResult, stdout string, err error) {
+					if err != nil {
+						t.Fatal(err)
+					}
+					result.AssertInfrastructureIsInSync()
+					result.AssertManagedCount(1)
+				},
+			},
+		},
+	})
+}
