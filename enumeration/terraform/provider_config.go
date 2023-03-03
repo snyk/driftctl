79faae2242d8220a0a04449ee78fa005d43f5f0e
h-dav <harry.cr.davidson@gmail.com>
2023-03-03 19:52:20 +0000

    Incorrect terraform provider downloaded
    If runtime.GOARCH was "arm64" then would get set to "amd64"? Possible mistake?
    
    Closes #1632

--- enumeration/terraform/provider_config.go
+++ enumeration/terraform/provider_config.go
@@ -1,32 +1,29 @@
 package terraform
 
 import (
-	"fmt"
-	"runtime"
+    "fmt"
+    "runtime"
 )
 
 type ProviderConfig struct {
-	Key       string
-	Version   string
-	ConfigDir string
+    Key       string
+    Version   string
+    ConfigDir string
 }
 
 func (c *ProviderConfig) GetDownloadUrl() string {
-	arch := runtime.GOARCH
-	if runtime.GOOS == "darwin" && runtime.GOARCH == "arm64" {
-		arch = "amd64"
-	}
-	return fmt.Sprintf(
-		"https://releases.hashicorp.com/terraform-provider-%s/%s/terraform-provider-%s_%s_%s_%s.zip",
-		c.Key,
-		c.Version,
-		c.Key,
-		c.Version,
-		runtime.GOOS,
-		arch,
-	)
+    arch := runtime.GOARCH
+    return fmt.Sprintf(
+        "https://releases.hashicorp.com/terraform-provider-%s/%s/terraform-provider-%s_%s_%s_%s.zip",
+        c.Key,
+        c.Version,
+        c.Key,
+        c.Version,
+        runtime.GOOS,
+        arch,
+    )
 }
 
 func (c *ProviderConfig) GetBinaryName() string {
-	return fmt.Sprintf("terraform-provider-%s_v%s", c.Key, c.Version)
+    return fmt.Sprintf("terraform-provider-%s_v%s", c.Key, c.Version)
 }
