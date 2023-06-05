7eec7e2342c53112ce33177da01a0923bdb909ca
Craig Furman <craig.furman89@gmail.com>
2023-06-05 17:15:04 +0100

    chore: don't disable acc test KMS keys
    When applying acceptance test terraform for AWS KMS key resources, we
    often see errors of the form: `Failed to set KMS key ... status to
    false: "NotFoundException: Key '...' does not exist"`. Our current
    theory is that setting is_enabled=false (as we currently do) is causing
    terraform to have to perform 2 API operations: create the key, then
    disable it. Due to the "eventualy consistency" of this API we see this
    flaking quite often.
    
    We don't actually need to disable the keys, so let's not, in an attempt
    to improve the reliability of this test.

--- pkg/resource/aws/testdata/acc/aws_kms_key/terraform.tf
+++ pkg/resource/aws/testdata/acc/aws_kms_key/terraform.tf
@@ -10,23 +10,20 @@ terraform {
 
 resource "aws_kms_key" "foo" {
   description              = "Foo"
-  deletion_window_in_days = 7
-  is_enabled              = false
+  deletion_window_in_days  = 7
   customer_master_key_spec = "RSA_4096"
 }
 
 resource "aws_kms_key" "bar" {
   description              = "Bar"
-  deletion_window_in_days = 7
-  is_enabled              = false
+  deletion_window_in_days  = 7
   customer_master_key_spec = "RSA_2048"
-  key_usage = "SIGN_VERIFY"
+  key_usage                = "SIGN_VERIFY"
 }
 
 resource "aws_kms_key" "baz" {
   description             = "Baz"
   deletion_window_in_days = 7
-  is_enabled              = false
   tags = {
     "Foo" = "true"
   }
